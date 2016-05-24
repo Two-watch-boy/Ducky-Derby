809a54c960389c0a5ecf03f3d1a4063c9d9da49e
Two-Watch-Boy <twowatchboy@gmail.com>
2016-05-24 10:20:20 -0700

    made app responsive

--- scripts/app.js
+++ scripts/app.js
@@ -5,8 +5,8 @@ $(document).ready(function(){
   // $( "html" ).keydown(function() {
   //   alert( "Handler for .keydown() called." );
   // });
-  var duck1MarL = 30;
-  var duck2MarL = 30;
+  var duck1MarL = 2;
+  var duck2MarL = 2;
   var duck1Wins = 0;
   var duck2Wins = 0;
   var quack = new Audio('images/quack.mp3');
@@ -15,17 +15,17 @@ $(document).ready(function(){
   var over = false;
     $("html").on("keyup",function(event){
       if(over === true)return;
-      if(duck1MarL % 150 === 0){
+      if(duck1MarL % 16 === 0){
         ducky.play();
       }
       console.log(event);
       console.log("keydown");
       if(event.keyCode == 70){
-        if(duck1MarL < 1100){
-          duck1MarL += 10; // every click add 10 to i
-          $(".duck1").css("margin-left", duck1MarL);
+        if(duck1MarL < 88){
+          duck1MarL += 2; // every click add 2 to i
+          $(".duck1").css("margin-left", (duck1MarL + "%"));
           console.log(duck1MarL);
-          if(duck1MarL === 1100){
+          if(duck1MarL === 88){
             duck1Wins++;
             over = true;
             $(".winner").append('<p id="winner"> DUCK 1 WINS! </p>');
@@ -34,16 +34,16 @@ $(document).ready(function(){
         }
       }else if(event.keyCode == 74){
         if(over === true)return;
-        if(duck2MarL % 150 === 0){
+        if(duck2MarL % 16 === 0){
           ducky.play();
         }
         console.log(event);
         console.log("keydown");
-        if(duck2MarL < 1100){
-          duck2MarL += 10; // every click add 10 to i
-          $(".duck2").css("margin-left", duck2MarL);
+        if(duck2MarL < 88){
+          duck2MarL += 2; // every click add 2 to i
+          $(".duck2").css("margin-left", (duck2MarL + "%"));
           console.log(duck2MarL);
-          if(duck2MarL === 1100){
+          if(duck2MarL === 88){
             duck2Wins++;
             over = true;
             $(".winner").append('<p id="winner"> DUCK 2 WINS! </p>');
@@ -52,16 +52,16 @@ $(document).ready(function(){
         }
       }else if(event.keyCode == 49){
         if(over === true)return;
-        if(duck2MarL % 150 === 0){
+        if(duck2MarL % 7 === 0){
           ducky.play();
         }
         console.log(event);
         console.log("keydown");
-        if(duck2MarL < 1100){
-          duck2MarL += 100; // every click add 10 to i
+        if(duck2MarL < 88){
+          duck2MarL += 86; // every click add 2 to i
           $(".duck2").css("margin-left", duck2MarL);
           console.log(duck2MarL);
-          if(duck2MarL === 1100){
+          if(duck2MarL === 88){
             duck2Wins++;
             over = true;
             $(".winner").append('<p id="winner"> DUCK 2 WINS! </p>');
@@ -73,8 +73,10 @@ $(document).ready(function(){
     $(".reset").on("mousedown", function(){
       console.log("reset");
       quack.play();
-      $('.duck1').css("margin-left", duck1MarL = 30);
-      $('.duck2').css("margin-left", duck2MarL = 30);
+      duck1MarL = 2;
+      duck2MarL = 2;
+      $('.duck1').css("margin-left", (duck1MarL + "%"));
+      $('.duck2').css("margin-left", (duck2MarL + "%"));
       $('#winner').remove();
       over = false;
     });
@@ -84,8 +86,10 @@ $(document).ready(function(){
       $("#score").text('0 :SCORE: 0');
       duck1Wins = 0;
       duck2Wins = 0;
-      $('.duck1').css("margin-left", duck1MarL = 30);
-      $('.duck2').css("margin-left", duck2MarL = 30);
+      duck1MarL = 2;
+      duck2MarL = 2;
+      $('.duck1').css("margin-left", (duck1MarL + "%"));
+      $('.duck2').css("margin-left", (duck2MarL + "%"));
       $('#winner').remove();
       over = false;
     });
